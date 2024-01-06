c1c786aa554f8412b80861db34b0dfa9522c717b
Disha Srivastava <connectdisha08@gmail.com>
2024-01-06 20:52:53 +0530

    Delete Database connectivity and Create Database.py

--- Database connectivity and Create Database.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host='localhost' ,user='root', password='113008')
-print(mydb.connection_id)
-cur=mydb.cursor()
-cur.execute("CREATE DATABASE db1")
-
-
